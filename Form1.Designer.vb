e2ba24d0444d354b26e0e2a45e74d824109215c3
Northbridgeangel <67518040+Northbridgeangel@users.noreply.github.com>
2022-08-17 10:01:56 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.COMBODESORDEN = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.COMBOORDEN = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(8, 88)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(136, 40)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "BURBUJA"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'COMBODESORDEN
+        '
+        Me.COMBODESORDEN.FormattingEnabled = True
+        Me.COMBODESORDEN.Location = New System.Drawing.Point(256, 56)
+        Me.COMBODESORDEN.Name = "COMBODESORDEN"
+        Me.COMBODESORDEN.Size = New System.Drawing.Size(152, 21)
+        Me.COMBODESORDEN.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(256, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "VECTOR DESORDENADO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(424, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(131, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "VECTOR ORDENADO"
+        '
+        'COMBOORDEN
+        '
+        Me.COMBOORDEN.FormattingEnabled = True
+        Me.COMBOORDEN.Location = New System.Drawing.Point(424, 56)
+        Me.COMBOORDEN.Name = "COMBOORDEN"
+        Me.COMBOORDEN.Size = New System.Drawing.Size(152, 21)
+        Me.COMBOORDEN.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(8, 136)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(136, 40)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "INSERCION"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(8, 312)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(136, 40)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "BUSCAR"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(160, 312)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(128, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(160, 336)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(128, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(8, 184)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(136, 40)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "QUICKSORT"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(192, 8)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(560, 26)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Metodos de Ordenacion de Vectores y Busqueda Binaria"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("DS-Digital", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Yellow
+        Me.Label4.Location = New System.Drawing.Point(152, 88)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 36)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.COMBOORDEN)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.COMBODESORDEN)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "METODOS DE ORDENACION"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents COMBODESORDEN As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents COMBOORDEN As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,217 @@
+﻿Public Class Form1
+    Public DESORDEN() As Integer
+    Public ORDEN() As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        '*****************************************************************************
+        '*************************   METODO BURBUJA  *********************************
+        '*****************************************************************************
+        ' FASE 1 VARIABLES Y CARGA EN EL VECTOR NUMEROS AL TUN TUN
+        Cursor.Current = Cursors.WaitCursor
+        ReDim DESORDEN(100000)
+        ReDim ORDEN(100000)
+        COMBODESORDEN.Items.Clear()
+        COMBOORDEN.Items.Clear()
+        Dim POSI As Integer
+        Dim MINIMO As Integer
+        Randomize()
+        For I = 1 To 10000
+            DESORDEN(I) = Int(Rnd() * 4999 + 1)
+            COMBODESORDEN.Items.Add(DESORDEN(I))
+        Next
+        ' FASE 2 ORDENACION POR BURBUJA
+        Dim INICIO1, INICIO2 As Date
+        Dim segundos As String
+        INICIO1 = TimeOfDay
+        For I = 1 To 10000
+            MINIMO = 9999
+            POSI = 0
+            For X = 1 To 10000
+                If DESORDEN(X) < MINIMO Then
+                    MINIMO = DESORDEN(X)
+                    POSI = X
+                End If
+
+            Next
+
+            ORDEN(I) = MINIMO
+            DESORDEN(POSI) = 9999
+        Next
+        INICIO2 = TimeOfDay
+        segundos = DateDiff(DateInterval.Second, CDate(INICIO1), CDate(INICIO2))
+        Label4.Text = segundos
+        ' FASE 3 CARGAR EN COMBOORDEN EL VECTOR ORDEN
+        For I = 1 To 10000
+            COMBOORDEN.Items.Add(ORDEN(I))
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        '*****************************************************************************
+        '*************************   METODO INSERCION  *******************************
+        '*****************************************************************************
+        ' FASE 1 VARIABLES Y CARGA EN EL VECTOR NUMEROS AL TUN TUN
+        Cursor.Current = Cursors.WaitCursor
+        ReDim DESORDEN(10000)
+        ReDim ORDEN(10000)
+        COMBODESORDEN.Items.Clear()
+        COMBOORDEN.Items.Clear()
+        Randomize()
+        For X = 1 To 10000
+            DESORDEN(X) = Int(Rnd() * 4999 + 1)
+            COMBODESORDEN.Items.Add(DESORDEN(X))
+        Next
+        ' FASE 2 ORDENACION POR INSERCION LO HACE EN EL VECTOR DESORDEN
+        Dim INICIO1, INICIO2 As Date
+        Dim segundos As String
+        INICIO1 = TimeOfDay
+        Dim i As Long
+        Dim t As Integer
+        Dim Actual As Long
+        Dim Min As Long
+        Dim Max As Long
+        Dim ORDE As Integer
+        ORDE = 1
+        Min = LBound(DESORDEN)
+        Max = UBound(DESORDEN)
+
+        Actual = Min + 1
+        While Actual <= Max
+            i = Actual
+            Do
+                If i > Min Then
+                    If ORDE = 1 Then
+                        If DESORDEN(i) < DESORDEN(i - 1) Then
+                            t = DESORDEN(i)
+                            DESORDEN(i) = DESORDEN(i - 1)
+                            DESORDEN(i - 1) = t
+                            i = i - 1
+                        Else
+                            Exit Do
+                        End If
+                    Else
+                        If DESORDEN(i) >= DESORDEN(i - 1) Then
+                            t = DESORDEN(i)
+                            DESORDEN(i) = DESORDEN(i - 1)
+                            DESORDEN(i - 1) = t
+                            i = i - 1
+                        Else
+                            Exit Do
+                        End If
+                    End If
+                Else
+                    Exit Do
+                End If
+
+            Loop
+            Actual = Actual + 1
+        End While
+        INICIO2 = TimeOfDay
+        segundos = DateDiff(DateInterval.Second, CDate(INICIO1), CDate(INICIO2))
+        Label4.Text = segundos
+        ' FASE 3 CARGAR EN COMBOORDEN EL VECTOR ORDEN
+        For i = 1 To 10000
+            COMBOORDEN.Items.Add(DESORDEN(i))
+            ORDEN(i) = DESORDEN(i)
+        Next
+
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        '*****************************************************************************
+        '*************************   BUSQUEDA BINARIA  *******************************
+        '*****************************************************************************
+        Dim sup, inf, m As Integer
+        Dim DATO As Integer
+        DATO = InputBox("Mete el Dato")
+        inf = LBound(ORDEN)
+        sup = UBound(ORDEN)
+        Do
+            m = (inf + sup) / 2
+            If (DATO < ORDEN(m)) Then
+                sup = m
+            ElseIf (DATO > ORDEN(m)) Then
+                inf = m
+            End If
+        Loop Until (dato = ORDEN(m) Or inf >= sup)
+
+        If (dato = ORDEN(m)) Then
+            TextBox1.Text = “La posición es:” & m
+            TextBox2.Text = “El dato es: ” & ORDEN(m)
+        Else
+            TextBox1.Text = “El dato no se encontro”
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        '*****************************************************************************
+        '*******************************   QUICK SORT  *******************************
+        '*****************************************************************************
+        ' FASE 1 VARIABLES Y CARGA EN EL VECTOR NUMEROS AL TUN TUN
+        Cursor.Current = Cursors.WaitCursor
+        ReDim DESORDEN(100000)
+        ReDim ORDEN(100000)
+        COMBODESORDEN.Items.Clear()
+        COMBOORDEN.Items.Clear()
+        Randomize()
+        For X = 1 To 100000
+            DESORDEN(X) = Int(Rnd() * 4999 + 1)
+            COMBODESORDEN.Items.Add(DESORDEN(X))
+        Next
+        ' 2 FASE LLAMADA A LA FUNCION
+        Dim INICIO1, INICIO2 As Date
+        Dim segundos As String
+        INICIO1 = TimeOfDay
+        Call Ordenar_Matriz(DESORDEN, LBound(DESORDEN), UBound(DESORDEN))
+        INICIO2 = TimeOfDay
+        segundos = DateDiff(DateInterval.Second, CDate(INICIO1), CDate(INICIO2))
+        Label4.Text = segundos
+        ' FASE 3 CARGAR EN COMBOORDEN EL VECTOR ORDEN
+        For I = 1 To 100000
+            COMBOORDEN.Items.Add(DESORDEN(I))
+            ORDEN(I) = DESORDEN(I)
+        Next
+    End Sub
+    Sub Ordenar_Matriz(El_Vector() As Integer,
+                   Limite_Inferior As Long,
+                   Limite_Superior As Long)
+
+        Dim i As Long, j As Long, x As Integer, y As Integer
+
+        i = Limite_Inferior
+        j = Limite_Superior
+
+        x = El_Vector((Limite_Inferior + Limite_Superior) / 2)
+
+        While i <= j
+
+            While (El_Vector(i) < x) And (i < Limite_Superior)
+                i = i + 1
+            End While
+
+            While (x < El_Vector(j)) And (j > Limite_Inferior)
+                j = j - 1
+            End While
+
+            If i <= j Then
+                y = El_Vector(i)
+                El_Vector(i) = El_Vector(j)
+                El_Vector(j) = y
+                i = i + 1
+                j = j - 1
+            End If
+
+        End While
+
+        If Limite_Inferior < j Then Ordenar_Matriz(El_Vector, Limite_Inferior, j)
+        If i < Limite_Superior Then Ordenar_Matriz(El_Vector, i, Limite_Superior)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label4.Text = Val(Label4.Text) + 1
+    End Sub
+End Class
